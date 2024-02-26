cac0e8e9e80edce559fa3e6598b5db74cbc4f5ae
berkayagl <156705760+berkayagl@users.noreply.github.com>
2024-02-26 17:58:21 +0300

    Create literal.go

---
+++ literal.go
@@ -0,0 +1,13 @@
+
+package main
+
+import "fmt"
+
+func main() {
+
+	m := map[string]string{"name": "Berkay", "age": "20", "status": "Busy"}
+
+	fmt.Println(m)
+}
+
+// Çıktısı : map[age:20 name:Berkay status:Busy]
